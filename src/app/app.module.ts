1f29ce34595b6a8f51fc6ab3469aff2f4d54418c
MASHa <MA77777SHa>
2021-07-26 13:17:58 +0300

    cleaner master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
